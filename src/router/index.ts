61e241bfb485bfdf74e8c02b47716fbc518be7e8
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-07-12 16:03:02 +0200

    todoview et route créés

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TodoView from "@/views/TodoView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+  { 
+    path: "/",
+    component: TodoView
+  }
   ]
 })
 
