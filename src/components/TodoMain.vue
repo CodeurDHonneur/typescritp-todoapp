211b4a2c579496762a03c911ef958bdc0641279e
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-07-12 17:48:37 +0200

    TodoMain créé

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="main">
+    <ul class="todo-list">
+      <TodoItem v-for="todo in todos" :key="todo.id" :todo="todo" />
+    </ul>
+  </main>
+</template>
+
+<script setup lang="ts">
+import type { Todo } from '@/@types'
+import TodoItem from '@/components/TodoItem.vue'
+
+// const props = defineProps(['todos']);
+const props = defineProps<{
+  todos: Todo[]
+}>()
+</script>
+
+<style scoped></style>
