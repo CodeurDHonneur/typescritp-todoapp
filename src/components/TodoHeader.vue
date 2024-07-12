ef1bc8eae694a4347bfa5318f542ed111df4646e
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-07-12 16:47:04 +0200

    TodoHeader créé

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="header">
+    <RouterLink to="/">
+      <h1>todos</h1>
+    </RouterLink>
+
+    <input
+      type="text"
+      placeholder="Entrez une tâche"
+      class="new-todo"
+      autofocus
+      autocomplete="off"
+    />
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
